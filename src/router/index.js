93bd91f74301821b02f4e296863afa2d869f23de
nzeyvan <nzeyvan@hotmail.fr>
2020-12-06 01:45:06 +0100

    Generator QrCode

--- src/router/index.js
+++ src/router/index.js
@@ -41,6 +41,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/RegisterEstablishment.vue')
+  },
+  {
+    path: '/homeDoctor',
+    name: 'HomeDoctor',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/HomeDoctor.vue')
+  },
+  {
+    path: '/homeEstablishment',
+    name: 'HomeEstablishment',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/HomeEstablishment.vue')
   }
 ]
 
---
+++ src/views/HomeEstablishment.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class = "doctor">
+        <button type="button" v-on:click="generate" id="generate">Genérer un QRCode</button>
+        <div id="qr">
+            <vue-qrcode id="qrcode" value="" v-model= "id_qr" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import VueQrcode from 'vue-qrcode'
+    
+    export default {
+        data(){
+            return{
+                id_qr :''
+            }
+        },
+        components: {
+            VueQrcode,
+        },
+        methods: {
+            generate: function () {
+                this.id_qr = String(Math.floor(Math.random() * 1000000));
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
